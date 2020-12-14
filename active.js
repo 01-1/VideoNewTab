2d91a8487ecbb9bef38ffe13349b27c45658fd8e
Unum <33045716+etaui@users.noreply.github.com>
2020-12-13 19:44:33 -0500

    Added shuffle playlist

--- active.js
+++ active.js
@@ -1,10 +1,18 @@
+function shuffleArray(array) {
+    // https://stackoverflow.com/a/12646864/9245282
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 
 window.onload = () => {
     chrome.storage.sync.get(["ytLinks"], function(data) {
         var links = data["ytLinks"];
         if (links !== undefined) {
-            var randomChoice = links[Math.floor(Math.random()*links.length)]
-            document.getElementById("mainVideo").setAttribute("src",`https://www.youtube.com/embed/${randomChoice}`);
+            shuffleArray(links);
+            document.getElementById("mainVideo").setAttribute("src",`https://www.youtube.com/embed/${links[0]}?playlist=`+links.slice(1).join(','));
         }
     });
 }
